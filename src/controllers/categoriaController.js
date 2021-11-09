927198dab34096b5b7bdffa5e0438f43edcde32b
Thiago Moreira <thiagomoreira.ifsc@gmail.com>
2021-11-09 15:19:54 -0300

    funcionalidades adicionadas ao controller categoria

--- src/controllers/categoriaController.js
+++ src/controllers/categoriaController.js
@@ -1,14 +1,45 @@
 const Categoria = require('../models/categoria');
 
-class CatergoriaController {
-    async criar(req, res){
+class CategoriaController {
+    async criar(req, res) {
         const categoria = await Categoria.create({
             nome: req.body.nome
         })
         return res.json(categoria);
     }
 
-    async alterar(){}
+    async listarTodos(req, res) {
+        const categoria = await Categoria.findAll();
+        return res.json(categoria);
+    }
 
-    async deletar(){}
+    async alterar(req, res) {
+        const busca = await Categoria.findByPk(req.body.id);
+        if (busca === null) {
+            return res.json("Categoria não encontrada");
+        } else {
+            const categoria = await Categoria.update({ nome: req.body.nome }, {
+                where: {
+                    id: req.body.id
+                }
+            });
+            return res.json(categoria);
+        }
+    }
+
+    async deletar(req, res) {
+        const busca = await Categoria.findByPk(req.body.id);
+        if (busca === null) {
+            return res.json("Categoria não encontrada");
+        } else {
+            const categoria = await Categoria.destroy({
+                where: {
+                    id: req.body.id
+                }
+            });
+            return res.json(categoria);
+        }
+    }
 }
+
+module.exports = new CategoriaController;
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const categoriaController = require('./controllers/categoriaController');
 
 const routes = express.Router();
+const CategoriaController = require('./controllers/categoriaController');
+
+routes.post('/criarCategoria', CategoriaController.criar);
+routes.get('/listarCategoria', CategoriaController.listarTodos);
+routes.post('/removerCategoria', CategoriaController.deletar);
+routes.post('/alterarCategoria', CategoriaController.alterar);
 
 routes.get('/', function(req,res){
     res.send('Teste');
