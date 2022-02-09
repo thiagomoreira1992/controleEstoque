b05dda9e06342c7eb4e4512262b76ed07789edba
Thiago Moreira <thiagomoreira.ifsc@gmail.com>
2022-02-09 18:01:22 -0300

    alterado controller movimentacao

--- src/controllers/movimentacaoController.js
+++ src/controllers/movimentacaoController.js
@@ -4,22 +4,28 @@ const Material = require('../models/material');
 class MovimentacaoController{
     
     async criar(req,res){
-        const movimentacao = await Movimentacao.create({
-            idMaterial: req.body.material,
-            quantidade : req.body.quantidade,
-            lote :req.body.lote,
-            profissional : req.body.profissional
-        })
-        const buscaMaterial = await Material.findByPk(req.body.material);
-        let operacao = parseInt(buscaMaterial.quantidade) + parseInt(req.body.quantidade);
-
-        const material = await Material.update({quantidade: operacao},{
-            where:{
-                id: req.body.material
-            }
-        })
-        return res.json(material + movimentacao);
-        //return res.json(movimentacao);
+        const buscaMaterial = await Material.findOne({where: {lote: `${req.body.lote}`}});
+        console.log(buscaMaterial);
+        if(buscaMaterial === null){
+            return res.json('Material não encontrado!');
+        }else{            
+            const movimentacao = await Movimentacao.create({
+                idMaterial: buscaMaterial.id,
+                quantidade : req.body.quantidade,
+                lote :req.body.lote,
+                profissional : req.body.profissional
+            })
+            
+            let operacao = parseInt(buscaMaterial.quantidade) + parseInt(req.body.quantidade);
+    
+            const material = await Material.update({quantidade: operacao},{
+                where:{
+                    id: buscaMaterial.id
+                }
+            })
+            return res.json(material + movimentacao);
+            //return res.json(movimentacao);
+        }
     }
     
     async listarTodos(req,res){
